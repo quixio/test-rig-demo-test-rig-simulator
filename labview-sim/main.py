1bd054950ddb99ffdb834b25e94381be93d09556
Steve Rosam <steve+quixers@quix.io>
2025-10-27 10:05:29 +0000

    Edited 'main.py'

--- labview-sim/main.py
+++ labview-sim/main.py
@@ -308,6 +308,8 @@ def home_page():
                 <div class="media-links">
                     <ul>
                         <li><a href="">Link 1</a></li>
+                        <li><a href="https://query-ui-quixers-testrigdemodatawarehouse-prod.az-france-0.app.quix.io/?token=sdk-5de13ff9d5f644b2a614adc6c5b9ac7a">Data warehouse</a></li>
+                        <li><a href="https://portal.cloud.quix.io/data?workspace=quixers-testrigdemomeasurementdata-prod">Data lake</a></li>
                         <li><a href="https://portal.cloud.quix.io/pipeline?workspace=quixers-testrigdemomeasurementdata-prod">Measurement data</a></li>
                         <li><a href="https://frontend-7bdbce0-quixers-testrigdemoconfigurationmet-5f306d01.az-france-0.app.quix.io/">Test Manager</a></li>
                         <li><a href="https://marimo-analysis-quixers-testrigdemoadvancedanalytics-prod.az-france-0.app.quix.io">Full Analysis</a></li>
